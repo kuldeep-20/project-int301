83b87af8c65105a5aa6734b89e6a7a20cca17bbd
sidv0910 <sid.verma0910@gmail.com>
2023-04-03 20:17:19 +0530

    Adding the method to capture HTTP packets

--- http-intercept.py
+++ http-intercept.py
@@ -9,3 +9,9 @@ tcp_port = "80"
 
 # display (wireshark) filter to apply on the Live Capture
 http_filter = "http and tcp.port == {port} and ip.addr == {ip}".format(port = tcp_port, ip = ip_address)
+
+# method to capture http packets with the display filter initialized above
+def intercept_http_packets(network_interface):
+    capture = pyshark.LiveCapture(interface=network_interface, display_filter=http_filter)
+    for packet in capture.sniff_continuously():
+        filter_layer_from_packet(packet)
