2d124a1013e5dc8e4e23243765f4395c6ea75cf9
sidv0910 <sid.verma0910@gmail.com>
2023-04-04 18:11:14 +0530

    Adding the method to filter HTTP packets

--- http-intercept.py
+++ http-intercept.py
@@ -15,3 +15,9 @@ def intercept_http_packets(network_interface):
     capture = pyshark.LiveCapture(interface=network_interface, display_filter=http_filter)
     for packet in capture.sniff_continuously():
         filter_layer_from_packet(packet)
+
+# method to filter the packets having the login form data
+def filter_layer_from_packet(packet):
+    for layer in packet.layers:
+        if layer.layer_name == "urlencoded-form":
+            extract_login_credentials_from_output_file(packet)
